177aec7dd2cb9e93bc5d4be1766715a92af7f4d0
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-02 19:49:33 +0430

    add(01-task):do the task to invistage matplotlib plots

---
+++ tasks/01_line_plot_basics.py
@@ -0,0 +1,7 @@
+# Difficulty: Easy
+# Task: Plot y = x² for x in [0, 10]. Add title, x-label, y-label, and grid.
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 10, 50)
+y = x ** 2
+# TODO: Create and show the plot
