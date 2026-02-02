4ede63d5a9240fe13043574916916fa4331eb1cf
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-02 21:31:10 +0430

    the first basic example of plots in matplotlib librart

--- mian.py
+++ mian.py
@@ -1,3 +1,8 @@
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
-print(matplotlib.__version__)
+years = np.array([2022,2023,2024,2025,2026])
+students = np.array([21, 43,33,9,16])
+
+plt.plot(years, students, marker='o')
+plt.show()
