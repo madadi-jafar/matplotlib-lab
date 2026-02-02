9a0dc76d397cf409d8bc5ce03fc5fffcc07636e6
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-02 22:01:26 +0430

    add(solution): for task 01

--- tasks/01_line_plot_basics.py
+++ tasks/01_line_plot_basics.py
@@ -5,3 +5,19 @@ import numpy as np
 x = np.linspace(0, 10, 50)
 y = x ** 2
 # TODO: Create and show the plot
+
+#Sulotion: Task1:
+
+# Create the plot
+plt.plot(x, y, color='tab:blue', linewidth=2, marker='o', markersize=5)
+
+# Add title and labels
+plt.title('Plot of y = x²')
+plt.xlabel("x graph")
+plt.ylabel("y graph")
+
+# Add grid 
+plt.grid(True, linestyle='--', alpha=0.6)
+
+# Show the plot
+plt.show()
