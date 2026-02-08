ceba30808abc92a55340e23a209c0dcf55614b6e
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-08 17:59:29 +0430

    add(task):day 2 - scatter plots review

---
+++ tasks/02_scatter_plot_simple.py
@@ -0,0 +1,8 @@
+# Difficulty: Easy
+# Task: Generate 50 random (x, y) points. Create a scatter plot with red circles.
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(0)
+x = np.random.rand(50)
+y = np.random.rand(50)
+# TODO: Make scatter plot with red "o" markers
