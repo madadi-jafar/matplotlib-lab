76a26836004ca0bf509b1fc23091c997b8aa730d
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-11 21:30:47 +0430

    add(task4): histogram basic

---
+++ tasks/04_histogram_simple.py
@@ -0,0 +1,7 @@
+# Difficulty: Easy
+# Task: Generate 1000 normal samples (mean=50, std=10). Plot histogram with 20 bins.
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(1)
+data = np.random.normal(50, 10, 1000)
+# TODO: Plot histogram
