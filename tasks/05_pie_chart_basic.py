826de61e140262fc46307b336b56187a2b65d430
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-11 21:32:15 +0430

    add(task5): pie chart basic

---
+++ tasks/05_pie_chart_basic.py
@@ -0,0 +1,6 @@
+# Difficulty: Easy
+# Task: Plot pie chart for ["Red", "Blue", "Green"] with sizes [40, 35, 25].
+import matplotlib.pyplot as plt
+labels = ["Red", "Blue", "Green"]
+sizes = [40, 35, 25]
+# TODO: Create pie chart with percentage labels
