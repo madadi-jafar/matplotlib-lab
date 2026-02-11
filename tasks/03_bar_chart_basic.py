a7aab7bc90e6db243bbdd1b2cbb6379d8f1dfd92
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-11 21:26:39 +0430

    add(day3): bar chart basic

---
+++ tasks/03_bar_chart_basic.py
@@ -0,0 +1,6 @@
+# Difficulty: Easy
+# Task: Plot bar chart for categories ["A", "B", "C"] with values [3, 7, 5].
+import matplotlib.pyplot as plt
+categories = ["A", "B", "C"]
+values = [3, 7, 5]
+# TODO: Create vertical bar chart
