f4491e9a8588e55bd7806e589ef88ded8b3192f2
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-17 21:59:09 +0430

    add(task-08):custom-colors

---
+++ tasks/08_custom_colors.py
@@ -0,0 +1,6 @@
+# Difficulty: Easy
+# Task: Plot y = x with blue line, y = 2x with green dashed line. Add legend.
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 5, 20)
+# TODO: Plot two lines with custom styles and legend
