18ce3e12b604ee2d9ef936fde1a9ec2959dbde90
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-17 21:07:03 +0430

    add(task-07):how to save figure

---
+++ tasks/07_save_figure.py
@@ -0,0 +1,7 @@
+# Difficulty: Easy
+# Task: Plot y = sqrt(x) for x in [0, 100]. Save as "sqrt_plot.png" at 150 DPI.
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 100, 100)
+y = np.sqrt(x)
+# TODO: Plot and save figure
