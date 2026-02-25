dd6fed4299166edf500f9a1043786316c911ac2e
Mahdi <hatamimahdi32@gmail.com>
2026-02-25 14:04:57 +0430

    box plots

--- tasks/task12/12_boxplot_comparison.py
+++ tasks/task12/12_boxplot_comparison.py
@@ -7,3 +7,12 @@ A = np.random.normal(70, 10, 100)
 B = np.random.normal(75, 5, 100)
 C = np.random.exponential(10, 100) + 60
 # TODO: Create grouped boxplot
+
+all_data = [A,B,C]
+colors = ["#DE1616FF", "#0400F0", "#F1F11B"]
+boxplot = plt.boxplot(all_data,notch=True,patch_artist=True)
+for patch, color in zip(boxplot['boxes'], colors):
+    patch.set_facecolor(color)
+    
+plt.xticks([1,2,3],['A','B','C'])
+plt.show()
