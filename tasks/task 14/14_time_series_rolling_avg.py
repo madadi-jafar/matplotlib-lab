637f11ee268a5940ad5794f02c0af429086b42f0
Mahdi <hatamimahdi32@gmail.com>
2026-02-28 22:19:14 +0430

    time series rolling average

--- tasks/task 14/14_time_series_rolling_avg.py
+++ tasks/task 14/14_time_series_rolling_avg.py
@@ -7,3 +7,11 @@ signal = 10 + np.sin(days/10) * 5
 noise = np.random.normal(0, 1, 100)
 data = signal + noise
 # TODO: Plot raw data, rolling avg, and uncertainty band
+avg = np.convolve(data, np.ones(7)/7, mode='same')
+std = np.std([np.roll(data, i) for i in range(7)], axis=0)
+
+plt.plot(days, data, alpha=0.3, label='Raw')
+plt.plot(days, avg, 'r', label='7d Avg')
+plt.fill_between(days, avg - std, avg + std, color='r', alpha=0.2)
+plt.legend()
+plt.show()
