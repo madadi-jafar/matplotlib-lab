239fba11927daa95f224eee6357f3b477629444a
Mahdi <hatamimahdi32@gmail.com>
2026-02-28 22:14:01 +0430

    heatmap correlation

--- tasks/task 13/13_heatmap_correlation.py
+++ tasks/task 13/13_heatmap_correlation.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 np.random.seed(4)
 # TODO: Create valid corr matrix and heatmap with annotations
+data = np.random.rand(5, 5)
+corr_matrix = np.corrcoef(data)
+plt.imshow(corr_matrix)
+for (i, j), z in np.ndenumerate(corr_matrix):
+    plt.text(j, i, '{:0.2f}'.format(z), ha='center', va='center')
+    
+plt.colorbar()
+plt.show()
