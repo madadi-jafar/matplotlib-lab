f05f949d98bf015f3a2e4fcee79c66c3282adcc7
Mahdi <hatamimahdi32@gmail.com>
2026-02-28 22:24:15 +0430

    style context manager

--- tasks/task 15/15_style_context_manager.py
+++ tasks/task 15/15_style_context_manager.py
@@ -5,3 +5,10 @@ import numpy as np
 x = np.linspace(0, 5, 50)
 y = x ** 2
 # TODO: Use plt.style.context in subplots
+for i, s in enumerate(['default', 'seaborn-v0_8']):
+    with plt.style.context(s):
+        ax = plt.subplot(1, 2, i + 1)
+        ax.plot(x, y, label=s)
+        ax.legend()
+
+plt.show()
